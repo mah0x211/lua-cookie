8e0de4eb8be65147e16b9ddeaaa01243a7e81038
Masatoshi Teruya <mah0x211@gmail.com>
2016-07-01 14:24:17 +0900

    Eliminate the trailing-whitespace

--- cookie.lua
+++ cookie.lua
@@ -1,5 +1,5 @@
 --[[
-  
+
   Copyright (C) 2014 Masatoshi Teruya
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -8,10 +8,10 @@
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
- 
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
- 
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -19,10 +19,10 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
-  
+
   cookie.lua
   Created by Masatoshi Teruya on 14/11/07.
-  
+
 --]]
 
 -- modules
@@ -33,7 +33,7 @@ local typeof = require('util').typeof;
 local EATTR = 'attr.%s must be %s';
 -- constants
 -- attribute names are not case-sensitive
-local ATTRS = { 
+local ATTRS = {
     domain = 'string',
     path = 'string',
     expires = 'int',
@@ -44,13 +44,13 @@ local ATTRS = {
 
 local function parseAttr( attr, callback )
     local v;
-    
+
     if not typeof.table( attr ) then
         return 'attr must be table';
     elseif not typeof.Function( callback ) then
         return 'callback must be function';
     end
-    
+
     for k, t in pairs( ATTRS ) do
         v = attr[k];
         if v ~= nil then
@@ -75,24 +75,24 @@ local Cookie = require('halo').class.Cookie;
 
 function Cookie.parse( cookies )
     local tbl = {};
-    
+
     if not cookies then
         return tbl;
     elseif not typeof.string( cookies ) then
         return nil, 'cookie must be string';
     end
-    
+
     for k, v in cookies:gmatch( '([^;%s]+)=([^;]+)' ) do
         tbl[k] = v;
     end
-    
+
     return tbl;
 end
 
 
 function Cookie.bake( name, val, attr )
     local c, err
-    
+
     -- with trim option
     name = isCookieName( name, true );
     if not name then
@@ -114,22 +114,22 @@ function Cookie.bake( name, val, attr )
             end
         end);
     end
-    
+
     if err then
         return nil, err;
     end
-    
+
     return c;
 end
 
 
 function Cookie:__index( field )
     local own = protected(self);
-    
+
     if field == 'name' then
         return own.name;
     end
-    
+
     return own.attr[field];
 end
 
@@ -138,7 +138,7 @@ function Cookie:init( name, attr )
     local own = protected(self);
     local tbl = {};
     local err;
-    
+
     if not isCookieName( name ) then
         err = 'name must be valid cookie-name string';
     elseif attr then
@@ -146,20 +146,20 @@ function Cookie:init( name, attr )
             tbl[k] = v;
         end);
     end
-    
+
     if err then
         return nil, err;
     end
     own.name = name;
     own.attr = tbl;
-    
+
     return self;
 end
 
 
 function Cookie:bake( val )
     local own = protected(self);
-    
+
     return Cookie.bake( own.name, val, own.attr );
 end
 
